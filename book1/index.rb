7d07dd1ef82d033c697d368a497a3b09b6a8dc78
Edgar Hovhannisyan <edgar.hovhannisyan08@gmail.com>
2020-01-31 19:59:59 +0400

    initial

--- book1/index.rb
+++ book1/index.rb
@@ -99,10 +99,10 @@ end
 # https://medium.com/@alexsnaumov/%D0%BF%D1%80%D0%BE%D0%BA%D0%B8-%D0%B1%D0%BB%D0%BE%D0%BA%D0%B8-%D0%B8-%D0%BB%D1%8F%D0%BC%D0%B1%D0%B4%D1%8B-%D0%B2-ruby-f81c403bd1f5
 
 print_param = lambda do |x, y| 
-    puts x + y 
+    puts x + y
 end
-
 # print_param.call(10, 5)
+
 
 # ============================ Date, Times ================================
 
@@ -130,8 +130,79 @@ class Fixnum
     end
 end
 
-puts Time.now
-puts Time.now + 10.minutes
-puts Time.now + 16.hours
-puts Time.now - 7.days
-puts 10.test
+# puts Time.now
+# puts Time.now + 10.minutes
+# puts Time.now + 16.hours
+# puts Time.now - 7.days
+# puts 10.test
+
+# ===================== Proc, Lambda ==========================
+
+# mr = Proc.new {puts "i'm mr Proc" }
+# ms = Proc.new {puts "I'm ms Proc" }
+
+# def hello(p1, p2)
+#     p1.call
+#     p2.call
+# end
+# hello(mr, ms)
+
+
+# def gen_times(factor)
+#     return Proc.new do |n|
+#         n * factor
+#     end
+# end
+
+# times3 = gen_times(3)
+# times5 = gen_times(5)
+
+# p times3.call(5)
+# p times5.call(5)
+
+# ======================= Ranges ===============================
+
+# (1..99).to_a.each do |let|
+#     p let
+# end
+
+# p ("A".."Z").include?("15")
+
+# arr = ["a", "b", "c", "x", "y", "z"]
+
+# new_arr = arr[3..5];
+# p new_arr, arr;
+# arr[0..2] = new_arr;
+# p arr
+
+# ======================= Symbols ==============================
+
+# curr_situation = :good
+# puts curr_situation.class
+
+# if curr_situation == :good
+#     puts "good"
+# else puts "bad"
+# end
+
+# curr_situation = :bad
+# if curr_situation == :good
+#     puts "good"
+# else puts "bad"
+# end
+
+
+# user1 = {
+#     :name => "Bob",
+#     :user => "Rodriges",
+#     :age => 20,
+#     :gender => :male
+# }
+
+# user1.default = false;
+# p user1["undefined key"]
+
+
+# ======================== Converting Objects to Other Classes ========================
+
+puts 12.to_s.to_i + 12.to_s.to_i
