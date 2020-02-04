7de38d6e78457deb164f1aee4f1610dc09f5d9d1
edgar <edgar.hovhannisyan08@gmail.com>
2020-02-04 23:43:03 +0400

    initial

--- book1/chapter5/index.rb
+++ book1/chapter5/index.rb
@@ -6,7 +6,7 @@ end
 
 def area_of_square(side_length)
     side_length * side_length
-endgi
+end
 
 def perimeter_of_triangle(side1, side2, side3)
     side1 + side2 + side3
@@ -79,21 +79,114 @@ end
 # basic_method
 # puts x
 
-# =================================== Global Variables ==================================
+# ================================ Global Variables =============================
 
-def basic_method
-    x = 777
-    puts $x
-end
-$x = 33
-x = 33
-basic_method
+# def basic_method
+#     x = 777
+#     puts $x
+# end
+# $x = 33
+# x = 33
+# basic_method
+
+# ======================== Instance or Object Variables =======================
+# class Test
+#     def initialize(obj_var)
+#         @obj_var = obj_var;
+#     end
+#     def test_method
+#         @obj_var
+#     end
+# end
+
+# test = Test.new("ok")
+# p test.test_method
 
 
+# ========================== Class Variables ===========================
 
+# class Test
+#     def initialize
+#         if defined?(@@objects_count)
+#             @@objects_count += 1
+#         elsif
+#             @@objects_count = 1
+#         end
+#     end
+#     def self.count
+#         @@objects_count
+#     end
+# end
 
+# a = Test.new
+# b = Test.new
+# c = Test.new
+# p Test.count
 
+# ==================== Class methods vs Instance methods =====================
 
+# class Test
+#     def self.test_method
+#         puts "Method of Class"
+#     end
+#     def test_method
+#         puts "Method of Instance"
+#     end
+# end
 
+# Test.test_method
+# Test.new.test_method
+
+# ======================== Inheritance =======================
+
+# class Person
+#     def initialize(name)
+#         @name = name
+#     end
+
+#     def getName
+#         @name
+#     end
+# end
+
+# class Programmer < Person
+#     def getName
+#         "Programmer: " + super.to_s
+#     end
+# end
+
+# # person = Person.new("Mark")
+# # puts person.getName
+
+# programmer = Programmer.new("Bob")
+# puts programmer.getName
+# print programmer.instance_variables
+
+# ====================== Overriding existing methods ======================
+
+# class String
+#     def length
+#         "new length!"
+#     end
+# end
+
+# str = "hello world"
+# puts str.length
+
+# ==================== Reflection and Discovering an Object’s Methods =====================
+
+# a = "This is a test"
+# print a.methods
+
+# class Person
+#     attr_accessor :name, :age
+# end
+
+# p = Person.new
+# p.name = "Fred"
+# p.age = 20
+# p.instance_variables
+
+# ============================ Encapsulation ==============================
 
 
