25d0dd87ed2659af5d7535e36dbb43250213f4d3
edgar <edgar.hovhannisyan08@gmail.com>
2020-02-09 18:37:28 +0400

    Initial

--- book1/chapter5/index.rb
+++ book1/chapter5/index.rb
@@ -461,30 +461,84 @@
 
 # ======================= Building a Dungeon ==========================
 
-Person = Struct.new(:name, :age, :gender)
-fred = Person.new("Fred", 20, "male")
-
 class Dungeon
     attr_accessor :player
     def initialize(player)
         @player = player
         @rooms = {}
     end
-end
 
-Player = Struct.new(:name, :location)
-
-Room = Struct.new(:reference, :name, :description, :connections)
-
-class Dungeon
     def add_room(reference, name, description, connections)
-        @rooms[:reference] = Room.new(reference, name, description, connections)
+        @rooms[reference] = Room.new(reference, name, description, connections)
+    end
+    
+    def start(location)
+        @player.location = location
+        show_current_description
+    end
+
+    def show_current_description
+        puts find_room_in_dungeon(@player.location).full_description
+    end
+
+    def find_room_in_dungeon(reference)
+        @rooms[reference]
+    end
+
+    def find_room_in_direction(direction)
+        find_room_in_dungeon(@player.location).connections[direction]
+    end
+
+    def go(direction)
+        puts "You go " + direction.to_s
+        @player.location = find_room_in_direction(direction)
+        show_current_description
     end
 end
 
-my_dungeon = Dungeon.new(fred)
+# Player = Struct.new(:name, :location)
+class Player
+    attr_accessor :name, :location 
+    def initialize(name)
+        @name = name
+        @location = location
+    end
+end
 
-my_dungeon.add_room(:largecave, "Large Cave", "a large cavernous cave", {:west => :smallcave })
-my_dungeon.add_room(:smallcave, "Small Cave", "a small, claustrophobic cave", {:east => :largecave })
+class Room
+    attr_accessor :reference, :name, :description, :connections
 
-puts my_dungeon.player.name
+    def initialize(reference, name, description, connections)
+        @reference = reference
+        @name = name
+        @description = description
+        @connections = connections
+    end
+
+    def full_description
+        @name + "\n\nYou are in " + @description
+    end
+end
+
+player = Player.new("Fred Bloggs")
+my_dungeon = Dungeon.new(player)
+
+my_dungeon.add_room(
+    :largecave,
+    "Large Cave",
+    "a large cavernous cave",
+    { :west => :smallcave })
+
+my_dungeon.add_room(
+    :smallcave,
+    "Small Cave",
+    "a small, claustrophobic cave",
+    { :east => :largecave })
+
+my_dungeon.start(:largecave)
+# my_dungeon.show_current_description
+
+my_dungeon.go(:west)
+
+my_dungeon.go(:east)
+
