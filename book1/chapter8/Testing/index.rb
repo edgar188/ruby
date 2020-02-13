00df023140c85e96db9ac9ba0aba1719e7d02537
edgar <edgar.hovhannisyan08@gmail.com>
2020-02-13 18:58:30 +0400

    Test Drive Dev.

--- book1/chapter8/Testing/index.rb
+++ book1/chapter8/Testing/index.rb
@@ -1,7 +1,15 @@
 class String
     def titleize
-        self.capitalize
+        # self.capitalize
+        # self.gsub(/\b\w/) {|letter| letter.upcase}
+        # self.gsub(/\s\w/) {|letter| letter.upcase}
+        self.gsub(/(\A|\s)\w/){ |letter| letter.upcase }
     end
 end
 
-puts "hello".titleize
+puts "hello world".titleize
+
+raise "Fail 1" unless "this is a test".titleize == "This Is A Test"
+raise "Fail 2" unless "another test 1234".titleize == "Another Test 1234"
+raise "Fail 3" unless "We're testing titleize".titleize == "We're Testing Titleize"
+
