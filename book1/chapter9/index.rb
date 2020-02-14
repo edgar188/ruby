02317d30b61ff6db49ac71d1534c8b59e41676c9
edgar <edgar.hovhannisyan08@gmail.com>
2020-02-14 18:56:09 +0400

    Directories

--- book1/chapter9/index.rb
+++ book1/chapter9/index.rb
@@ -2,16 +2,16 @@
 #     puts l.gets
 # end
 
-class MyFile
-    attr_reader :handle
-    def initialize(filename)
-        @handle = File.new(filename, "r")
-    end
+# class MyFile
+#     attr_reader :handle
+#     def initialize(filename)
+#         @handle = File.new(filename, "r")
+#     end
 
-    def finished
-        @handle.close
-    end
-end
+#     def finished
+#         @handle.close
+#     end
+# end
 
 # f = MyFile.new("text.txt")
 # f.handle.each do |line| 
@@ -52,6 +52,48 @@ end
 # ================ Delete File ====================
 
 # File.new("example.txt", "w")
+# File.delete("example.txt")
 
-File.delete("example.txt")
+# ================ File Operations ===================
+
+# print File.join('full', 'path', 'here', 'filename.txt')
+# print File.join(File::SEPARATOR , 'full', 'path', 'here', 'filename.txt')
+# print File.expand_path("tex1.txt")
+
+# puts File.mtime("text1.txt")
+# puts File.mtime("text1.txt").day
+# puts File.mtime("text1.txt").hour
+
+# ============ Checking Whether a File Exists =================
+# puts File.exist?("text2.txt") ? "File exists!" : "File not exist"
+
+# ============ Checking a File Size =================
+
+# print File.size("text1.txt") if File.exist?("text1.txt")
+# p File.size("text13.txt")
+
+# ============= Directories ================
+
+# Dir.chdir("./dir")
+# File.new("new.txt", "w")
+# puts Dir.getwd
+# puts Dir.pwd
+
+# # All files in current dir
+# puts Dir.entries("./").join(" ")
+
+# Dir.foreach("./") do |entry|
+#     puts entry
+# end
+
+# print Dir["./*"]
+# print Dir["./asd/*"]
+
+# Dir["./asd/*"].each do |file|
+#     File.delete(file)
+# end
+
+# Dir.delete("./asd")
+
+# ================ Creating Files in the Temporary Directory ==================
 
